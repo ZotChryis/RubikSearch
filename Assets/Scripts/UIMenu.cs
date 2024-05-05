d67899fcb2b1a1419e3cd3d4b3a5e8be9b6d9aee
ZotChryis <navac@outlook.com>
2024-05-05 00:20:31 -0700

    EVIL mode, hidden
    Also removes some bad patch note entry

--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class UIMenu : MonoBehaviour
 {
     [SerializeField] private RectTransform m_arrow;
+    [SerializeField] private  RectTransform m_target;
 
     public void PointArrowTo(RectTransform rectTransform)
     {
-        m_arrow.position = new Vector2(m_arrow.position.x, rectTransform.position.y);
+        m_target = rectTransform;
+    }
+
+    private void Update()
+    {
+        m_arrow.position = new Vector2(m_arrow.position.x, m_target.position.y);
     }
 }
